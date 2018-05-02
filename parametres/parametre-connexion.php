a83698dddc76f644375f722f554d50f2d88943f1
valentinozouf <32563089+valentinozouf@users.noreply.github.com>
2018-05-02 16:06:12 +0200

    Update parametre-connexion.php

--- parametres/parametre-connexion.php
+++ parametres/parametre-connexion.php
@@ -1,13 +1,13 @@
 <?php 
-	/*$user="acu_www_RW";
+	$user="acu_www_RW";
 	$mdp="kDMcbcUDi6rEqzBY";
 	$bddname="acu";
 	$host="mysql";
-	$dsn='mysql:host='.$host.';dbname='.$bddname;*/
+	$dsn='mysql:host='.$host.';dbname='.$bddname;
 
-	$user="root";
+	/*$user="root";
 	$mdp="admin";
 	$bddname="acuponcture";
 	$host="localhost";
-	$dsn='mysql:host='.$host.';dbname='.$bddname;
+	$dsn='mysql:host='.$host.';dbname='.$bddname;*/
 ?>
