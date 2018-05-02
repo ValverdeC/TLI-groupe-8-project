d09111488b4decf12ec14ffbd717f8389a3806ac
VALENTIN COUDOUR <SESA438330@schneider-electric.com>
2018-05-02 15:49:13 +0200

    mzeiucliuezh

--- parametres/parametre-connexion.php
+++ parametres/parametre-connexion.php
@@ -1,8 +1,14 @@
 <?php 
-	$user="acu_www_RW";
+	/*$user="acu_www_RW";
 	$mdp="kDMcbcUDi6rEqzBY";
 	$bddname="acu";
 	$host="mysql";
+	$dsn='mysql:host='.$host.';dbname='.$bddname;*/
+
+	$user="root";
+	$mdp="admin";
+	$bddname="acuponcture";
+	$host="localhost";
 	$dsn='mysql:host='.$host.';dbname='.$bddname;
 
 	/*$user="root";
