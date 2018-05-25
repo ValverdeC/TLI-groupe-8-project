8f8fbb3bfe3b77b2662f5d076cb9e3a8b054c5ee
Corentin VALVERDE <corentin-externe.valverde@edf.fr>
2018-05-25 17:13:47 +0200

    filter by category

--- src/app/pages/pathologies/pathologies.js
+++ src/app/pages/pathologies/pathologies.js
@@ -108,26 +108,31 @@ function filterByMeridienCode() {
     }
 }
 
+function filterByCaracteristic() {
+    var caracteristicSelector = document.getElementById("caracteristicSelector").value;
+
+    var infos = document.getElementsByClassName("infos");
+
+    if (caracteristicSelector != -1) {
+        for(i = 0; i < infos.length; i++) {
+            var type = infos[i].getElementsByClassName("descColumn");
+            if (!type[0].innerHTML.includes(caracteristicSelector)) {
+                infos[i].style.display = "none";
+            } else {
+                if (infos[i].style.display != "none") {
+                    infos[i].style.display = "";
+                }
+            }
+        }
+    }
+}
+
 function filter() {
     resetList();
     closeSymptoms();
     filterByMeridienCode();
     filterByType();
-
-    /*var input, filter, ul, li, a, i;
-    input = document.getElementById("myInput");
-    filter = input.value.toUpperCase();
-    ul = document.getElementById("myUL");
-    li = ul.getElementsByTagName("li");
-    for (i = 0; i < li.length; i++) {
-        a = li[i].getElementsByTagName("a")[0];
-        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
-            li[i].style.display = "";
-        } else {
-            li[i].style.display = "none";
-
-        }
-    }*/
+    filterByCaracteristic();
 }
 
 function resetList() {
